0b318c7b3a7b1455008b857c2f11ebb077f986b7
Matty1301 <matty_bates@live.co.uk>
2021-04-03 14:13:10 +0100

    Create MinionSpawnManager.cs

---
+++ MinionSpawnManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.LEGO.Behaviours.Triggers;   //For PickupTrigger type
+
+public class MinionSpawnManager : MonoBehaviour
+{
+    private List<Transform> spawnPoints = new List<Transform>();
+    public GameObject minionPrefab;
+    public int minionsToSpawn;
+    private GameObject[] aliveMinions;
+
+    public PickupTrigger loseTrigger;
+
+    void Start()
+    {
+        //Populate spawnPoints list with the transforms of the spawn point game objects
+        spawnPoints.AddRange(GetComponentsInChildren<Transform>());
+
+        //Remove the transform of the parent game object from the list
+        spawnPoints.Remove(transform);
+
+        aliveMinions = new GameObject[minionsToSpawn];
+    }
+
+    private void Update()
+    {
+        CheckAliveMinions();
+    }
+
+    //For each element in aliveMinions check if it is empty. If yes then spawn a new minion
+    void CheckAliveMinions()
+    {
+        for (int i = 0; i < minionsToSpawn; i++)
+        {
+            if (aliveMinions[i] == null)
+            {
+                aliveMinions[i] = SpawnMinion();
+            }
+        }
+    }
+
+    GameObject SpawnMinion()
+    {
+        //If there are no available spawn points you lose the game
+        if (spawnPoints.Count <= 0)
+            loseTrigger.Progress = 1;
+
+        int rand = Random.Range(0, spawnPoints.Count);  // Select a random spawn point
+        GameObject minionSpawned = Instantiate(minionPrefab, spawnPoints[rand]);
+        spawnPoints.Remove(spawnPoints[rand]);  //Remove the selected spawn point from the list of available spawn points
+        return minionSpawned;
+    }
+
+}
